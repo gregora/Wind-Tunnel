dba0ad3cd4effb63c58d4f1d450387da795be5c8
Gregor A <gregor372@gmail.com>
2024-07-15 23:42:49 +0200

    Improve analyzation visuals

--- data/analyze.py
+++ data/analyze.py
@@ -2,19 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+if(len(sys.argv) < 2):
+    print("Usage: python analyze.py <data file>")
+    sys.exit(1)
+
 file = sys.argv[1]
 
 # import data as csv
-
 data = np.genfromtxt(file, delimiter=',', skip_header=1)
 
 # plot data
 plt.subplot(2,1,1)
 plt.plot(data[:,0], data[:,1])
 plt.title("Lift vs. Time")
+plt.ylabel("Lift")
+plt.xlabel("Time [s]")
+
+plt.subplots_adjust(hspace=0.5)
 
 plt.subplot(2,1,2)
 plt.plot(data[:,0], data[:,2])
 plt.title("Drag vs. Time")
+plt.ylabel("Drag")
+plt.xlabel("Time [s]")
 
 plt.show()
