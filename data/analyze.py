5b61fe6e59a7cd10a492202f48744e896cbc0791
Gregor Alič <gregor372@gmail.com>
2024-10-23 21:30:46 +0200

    Update analyze.py to superinmpose multiple files

--- data/analyze.py
+++ data/analyze.py
@@ -3,27 +3,30 @@ import matplotlib.pyplot as plt
 import sys
 
 if(len(sys.argv) < 2):
-    print("Usage: python analyze.py <data file>")
+    print("Usage: python analyze.py <data file 1> <data file 2> <data file 3> ...")
     sys.exit(1)
 
-file = sys.argv[1]
+files = sys.argv[1:]
+datas = []
 
-# import data as csv
-data = np.genfromtxt(file, delimiter=',', skip_header=1)
+for file in files:
 
-# plot data
-plt.subplot(2,1,1)
-plt.plot(data[:,0], data[:,1])
-plt.title("Lift vs. Time")
-plt.ylabel("Lift")
-plt.xlabel("Time [s]")
+    # import data as csv
+    data = np.genfromtxt(file, delimiter=',', skip_header=1)
 
-plt.subplots_adjust(hspace=0.5)
+    # plot data
+    plt.subplot(2,1,1)
+    plt.plot(data[:,0], data[:,1])
+    plt.title("Lift vs. Time")
+    plt.ylabel("Lift")
+    plt.xlabel("Time [s]")
 
-plt.subplot(2,1,2)
-plt.plot(data[:,0], data[:,2])
-plt.title("Drag vs. Time")
-plt.ylabel("Drag")
-plt.xlabel("Time [s]")
+    plt.subplots_adjust(hspace=0.5)
+
+    plt.subplot(2,1,2)
+    plt.plot(data[:,0], data[:,2])
+    plt.title("Drag vs. Time")
+    plt.ylabel("Drag")
+    plt.xlabel("Time [s]")
 
 plt.show()
